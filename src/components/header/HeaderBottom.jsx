57419469369f15e67bde85b0be883900dc729221
mhnipu <mazedul0191.nipu@gmail.com>
2023-11-02 19:12:27 +0600

    sidebar updated

--- src/components/header/HeaderBottom.jsx
+++ src/components/header/HeaderBottom.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
-import { MenuRounded } from '@mui/icons-material'
+import { CloseFullscreen, CloseOutlined, MenuRounded } from '@mui/icons-material'
 import { Close } from '@mui/icons-material'
 import { AccountCircleRounded } from '@mui/icons-material'
 import SideNav from './SideNav'
+import { motion } from 'framer-motion'
 const HeaderBottom = () => {
     const [sidebar, setSideBar] = useState(false)
     return (
@@ -22,19 +23,44 @@ const HeaderBottom = () => {
             {/* sideNav start */}
             {
                 sidebar && (
-                    <div className='w-full h-screen text-black fixed top-0 left-0 bg-app_blue bg-opacity-50  '>
+                    <div className='w-full h-screen text-black fixed top-0 left-0 bg-app_blue backdrop-blur-sm bg-opacity-5 '>
                         <div className='w-full h-full relative '>
-                            <div className=' w-[350px] h-full border-0 border-app_light backdrop-blur-xl bg-white/50 '>
+                            <motion.div initial={{ x: -500, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: .5 }} className=' w-[350px] h-full border-0 border-app_light    backdrop-blur-xl bg-white/80 '>
                                 <div className='w-full bg-app_light text-white py-2 px-6 flex items-center gap-4 '>
                                     <AccountCircleRounded />
                                     <h3 className='font-titleFont font-bold text-lg tracking-wide'>Hello, Sign In</h3>
                                 </div>
                                 <div>
                                     <h3>
-                                        <SideNav />
+                                        <SideNav
+                                            title='Digital Contents & Devices'
+                                            one='Amazon Music' two='spotify' three='amazon Appstore' />
+                                        <SideNav
+                                            title='Books & Literature'
+                                            one='Bestsellers'
+                                            two='Mystery Novels'
+                                            three='Science Fiction'
+                                        />
+
+                                        <SideNav
+                                            title='Electronics & Gadgets'
+                                            one='Smartphones'
+                                            two='Wearable Tech'
+                                            three='Home Appliances'
+                                        />
+
+                                        <SideNav
+                                            title='Digital Contents & Devices'
+                                            one='E-books'
+                                            two='Streaming Music'
+                                            three='Mobile Apps'
+                                        />
+
                                     </h3>
                                 </div>
-                            </div>
+                                <span onClick={() => setSideBar(false)} className='cursor-pointer absolute top-2 left-[310px] w-8 h-8 text-whiteText flex items-center rounded justify-center 
+                                bg-app_light hover:bg-app_yellow hover:text-white duration-500'><CloseOutlined /></span>
+                            </motion.div>
 
                         </div>
                     </div>
--- src/components/header/SideNav.jsx
+++ src/components/header/SideNav.jsx
@@ -1,21 +1,36 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import { ArrowRightAltRounded } from '@mui/icons-material'
+import { CloseOutlined, KeyboardArrowRight } from '@mui/icons-material'
 import React from 'react'
 
-const SideNav = () => {
+const SideNav = ({ title, one, two, three }) => {
     return (
-        <div >
+        <div className='py-3 border-b-[1px] border-b-gray-300'>
             <h3 className="text-lg font-titleFont font-semibold mb-1 px-6">
-                Digital Contents & Devices
+                {title}
             </h3>
-            <ul>
-                <li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer">
-                    Amazon Music{''}
-                    <span>
-                        <ArrowRightAltRounded />
+            <ul className='text-sm font-semibold'>
+                <li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer ">
+                    <h2 className=" imgHover">{one}</h2>
+                    <span >
+                        <KeyboardArrowRight />
                     </span>
                 </li>
+                <li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer">
+                    <h2 className=" imgHover">{two}</h2>
+                    <span >
+                        <KeyboardArrowRight />
+                    </span>
+                </li>
+                <li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer">
+                    <h2 className=" imgHover">{three}</h2>
+                    <span >
+                        <KeyboardArrowRight />
+                    </span>
+                </li>
+
             </ul>
+            
         </div>
     )
 }
